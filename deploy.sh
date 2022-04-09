7cafa27b4982de110b3abd68e7420a3936d9da88
stellar124 <stellar124@gmail.com>
2022-04-09 03:19:47 +0200

    .

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 rm -rf deployed
+touch start
 
 #lehúzzuk a legfrisebbet
 cd ./repo
 git fetch --all
 git reset --hard origin/main
+
+mv ./start ./start_git
+
 ./../phptorun ./../composertorun install
+
+mv ./start_git ./start_git_comp
 
 cd ..
 dir_name=$(date +'%Y%m%d_%H%M%S')
@@ -12,6 +18,8 @@ dir_name=$(date +'%Y%m%d_%H%M%S')
 rm -rf releases/repo/
 #.git mappa kivételével átmásoljuk a lehúzott fájlokat (ez elég lassú lehet a cp és után aa .git mappa törlése jobb lenne)
 rsync -avq --progress ./repo ./releases --exclude .git
+
+mv ./start_git_comp ./start_git_comp_cp
 
 cd ./releases/repo
 
@@ -27,14 +35,20 @@ ln -s ./../../../shared/pub/generated ./pub/generated
 ln -s ./../../../shared/pub/media ./pub/media
 ln -s ./../../shared/var ./var
 
+mv ./start_git_comp_cp ./start_git_comp_cp_syml
+
  ./../../phptorun -dmemory_limit=-1 ./bin/magento cache:flush
  ./../../phptorun -dmemory_limit=-1 ./bin/magento cache:clean
  ./../../phptorun -dmemory_limit=-1 ./bin/magento setup:di:compile
  ./../../phptorun -dmemory_limit=-1 ./bin/magento setup:upgrade
  ./../../phptorun -dmemory_limit=-1 ./bin/magento setup:static-content:deploy
 
+mv ./start_git_comp_cp_syml ./start_git_comp_cp_syml_mag
+
 cd ../..
 
-rename ./releases/repo ./releases/${dir_name}
+mv ./releases/repo ./releases/${dir_name}
 unlink ./current
 ln -s ./releases/${dir_name} ./current
+
+mv ./start_git_comp_cp_syml_mag ./deployed
